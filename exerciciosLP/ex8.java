ee590cd309fe2950daa68becbfb07fcc2e79fc86
NickolasAranha <aranha.nickolas@gmail.com>
2026-01-13 17:01:38 -0300

    Exercicio 8

--- exerciciosLP/ex8.java
+++ exerciciosLP/ex8.java
@@ -7,15 +7,45 @@ public class ex8 {
         int valor1 = sc.nextInt();
         int valor2 = sc.nextInt();
         int valor3 = sc.nextInt();
+        int maior = 0;
+        int meio = 0;
+        int menor = 0;
 
-        int menor = valor1;
-        int maior = valor1;
-        if (menor > valor2) {
-            menor = valor2;
-        } else if (menor > valor3) {
-            menor = valor3;
-        } else if (maior < valor2) {
+
+        if(valor1 > valor2 && valor1 > valor3) {
+            maior = valor1;
+            if(valor2 < valor3) {
+                menor = valor2;
+                meio = valor3;
+            } else {
+                menor = valor3;
+                meio = valor2;
+            }
+        } else if (valor2 > valor1 && valor2 > valor3) {
             maior = valor2;
+            if(valor1 > valor3) {
+                meio = valor1;
+                menor = valor3;
+            } else {
+                meio = valor3;
+                maior = valor1;
+            }
+        } else {
+            maior = valor3;
+            if (valor1 > valor2) {
+                meio = valor1;
+                menor = valor2;
+            } else {
+                meio = valor2;
+                menor = valor1;
+            }
         }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Meio: " + meio);
+        System.out.println("Menor: " + menor);
+
+
+
     }
 }
